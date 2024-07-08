9592c67d4149f4b519b6bc250b795e2ab17c905e
Matt Hoskins <932722+matth0x1@users.noreply.github.com>
2024-07-08 15:36:16 +0100

    WIP experiment to try and generate the seed data as a migration.

---
+++ Nok.Infrastructure.Test.Database.Seeder/BogusExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Bogus;
+using Bogus.Extensions.UnitedKingdom;
+using Nok.Core.Aggregates.Register;
+
+namespace Nok.Infrastructure.Data.Migrations;
+
+internal static class BogusExtensions
+{
+    public static Address NokAddress(this Faker faker)
+        => new Address(
+            faker.Address.StreetAddress(),
+            faker.Address.SecondaryAddress(),
+            faker.Address.City(),
+            faker.Address.ZipCode(),
+            faker.Address.CountryOfUnitedKingdom()); // Everyone is in the UK for now
+
+    public static ContactDetails NokContactDetails(this Faker faker)
+        => new ContactDetails(
+            faker.Person.Email,
+            faker.Person.Phone,
+            faker.Person.Phone,
+            faker.Person.Phone);
+
+    public static DateOfBirth NokDateOfBirth(this Faker faker)
+    {
+        var dob = faker.Person.DateOfBirth;
+
+        return new DateOfBirth(dob.Year, dob.Month, dob.Day);
+    }
+
+    public static Vehicle NokVehicle(this Faker faker)
+        => new Vehicle(
+            faker.Vehicle.GbRegistrationPlate(new DateTime(2001, 09, 01), new DateTime(2024, 1, 1)),
+            faker.Vehicle.Manufacturer(),
+            faker.Vehicle.Model(),
+            faker.Commerce.Color(),
+            faker.Commerce.ProductDescription());
+
+
+}
---
+++ Nok.Infrastructure.Test.Database.Seeder/SeededDatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nok.Core.Aggregates.Register;
+using Nok.Infrastructure.Data;
+
+namespace Nok.Infrastructure.Test.Database.Seeder;
+
+public class SeededDatabaseContext : DatabaseContext
+{
+    private readonly SeedDataGenerator _seedDataGenerator;
+
+    public SeededDatabaseContext(DbContextOptions<DatabaseContext> options, SeedDataGenerator seedDataGenerator) : base(options)
+    {
+        _seedDataGenerator = seedDataGenerator;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Member>().HasData(_seedDataGenerator.Members);
+    }
+}
